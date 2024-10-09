f1b60881e3d09ccb80448262b88fecac71d3334a
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-10-09 11:20:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Choose Your Own Adventure
+
+This repository contains the code for a simple text-based Choose Your Own Adventure game. The game is written in C++ and allows the player to make choices that affect the outcome of the story.
+
+## Running the code
+
+To run the code, you will need to have a C++ compiler installed on your computer. You can use a compiler like g++ to compile the code.
+
+### Prerequisites
+
+- C++ compiler (e.g., g++)
+- Terminal or Command Prompt
+
+### Compiling from source
+
+To compile the code from source, follow these steps:
+
+1. Clone the repository to your local machine
+2. Navigate to the directory containing the source code
+3. Compile the code using g++ or another C++ compiler
+
+```bash
+g++ main.cpp -o main
+./main
+```
+
+## Contributing
+
+Contributions are not accepted at this time. This repository is for educational purposes only.
+
+## License
+
+This repository is licensed under the [Apache 2.0 License](LICENSE).
---
+++ src/main.cpp
@@ -0,0 +1,147 @@
+/*****
+ * Author: Glowstudent
+ * CYOA Game
+ *****/
+
+#include <iostream>
+#include <string>
+#include <limits>
+
+// https://stackoverflow.com/questions/70879879/how-can-i-make-a-rectangle-with-text-in-the-center-in-c
+// https://stackoverflow.com/questions/31396911/c-print-a-box-application
+// https://stackoverflow.com/questions/23369503/get-size-of-terminal-window-rows-columns
+// https://cplusplus.com/forum/general/175000/
+// https://www.codeproject.com/Questions/543606/Howplustoplusdrawplusatplusborderplusofplusscreenp
+// https://cplusplus.com/forum/beginner/28341/
+// https://stackoverflow.com/questions/421860/capture-characters-from-standard-input-without-waiting-for-enter-to-be-pressed
+// https://stackoverflow.com/questions/1761626/weighted-random-numbers
+// https://cplusplus.com/forum/general/272102/
+// https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Something
+
+using namespace std;
+
+void titleScreen();
+void credits();
+void resetInput();
+void getInt(int &input, int program, int min, int max, string cmessage, bool clearOnFail);
+
+void clearScreen()
+{
+	// Clear the screen
+#if defined(_WIN32) || defined(_WIN64)
+	system("cls");
+#elif defined(__linux__)
+	system("clear");
+#elif defined(__APPLE__)
+	system("clear");
+#endif
+}
+
+void resetInput()
+{
+	cin.clear();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+void getScreen(int screen)
+{
+	switch (screen)
+	{
+	case 1:
+		titleScreen();
+		break;
+	case 3:
+		credits();
+		break;
+	}
+}
+
+void getInt(int &input, int screen, int min, int max, string cmessage = "", bool clearOnFail = false)
+{
+	if (cmessage != "")
+	{
+		cout << cmessage;
+	}
+	else
+	{
+		cout << "Enter your choice: ";
+	}
+	cin >> input;
+
+	while (cin.fail() || input < min || input > max)
+	{
+		if (clearOnFail == true)
+		{
+			clearScreen();
+			getScreen(screen);
+		}
+		resetInput();
+		cout << "Invalid Input. Please enter a valid input: ";
+		cin >> input;
+	}
+}
+
+void titleScreen()
+{
+	int choice;
+
+	/*****
+	 * ASCII Art or something on title
+	 * 1) Play or Start Game
+	 * 2) Controls or Help
+	 * 3) Credits
+	 *****/
+
+	clearScreen();
+
+	cout << "===============================\n";
+	cout << "   CHOOSE YOUR OWN ADVENTURE   \n";
+	cout << "===============================\n";
+	cout << "1. Start Adventure\n";
+	cout << "2. Instructions\n";
+	cout << "3. Credits\n";
+	cout << "4. Exit\n";
+	cout << "===============================\n";
+
+	getInt(choice, 1, 1, 4, "Enter your choice (1-4): ");
+
+	switch (choice)
+	{
+	case 1:
+		cout << "Starting Adventure...\n";
+		break;
+	case 2:
+		cout << "Instructions...\n";
+		break;
+	case 3:
+		getScreen(3);
+		break;
+	case 4:
+		cout << "Exiting...\n";
+		break;
+	}
+}
+
+void credits()
+{
+	clearScreen();
+	resetInput();
+
+	cout << "===============================\n";
+	cout << "            CREDITS            \n";
+	cout << "===============================\n";
+	cout << "Author: Glowstudent\n";
+	cout << "===============================\n";
+	cout << "Press any key to return to the main menu...";
+	cin.get();
+	getScreen(1);
+}
+
+int main()
+{
+
+	clearScreen();
+	getScreen(1);
+
+	return 0;
+}
